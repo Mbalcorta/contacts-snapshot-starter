44fdf3d0fa5c88c116700d0696ce9a1c2499216c
Mateo Balcorta <misterrattail@gmail.com>
2018-01-25 15:06:23 -0800

    Add process.env variable.

--- src/models/db/db.js
+++ src/models/db/db.js
@@ -1,6 +1,14 @@
 const pgp = require('pg-promise')()
 
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/contacts_development'
-const db = pgp(connectionString)
+let connectionString
+
+if(process.env.NODE_ENV === 'test'){
+  connectionString =  'postgres://localhost:5432/contacts_test'
+} else {
+  // connectionString = 'postgres://localhost:5432/contacts_test'
+  connectionString = process.env.DATABASE_URL
+}
+
+const db = pgp({connectionString})
 
 module.exports = db
